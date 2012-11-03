e44f2e3f2da9be5e39a33f974675f5f70337ba70
Johannes Kaefer <jak4@jak4.com>
2012-11-03 19:35:43 +0100

    first status incarnation

--- lib/progress_x/status.rb
+++ lib/progress_x/status.rb
@@ -2,6 +2,20 @@
 
 module ProgressX
   module Status
+    extend ActiveSupport::Concern
 
+    included do
+      attr_accessor :att, :limitt
+    end
+
+    module ClassMethods
+    end
+
+    module InstanceMethods
+      def inc(v)
+        self.att  += v
+        self.att
+      end
+    end
   end
 end
